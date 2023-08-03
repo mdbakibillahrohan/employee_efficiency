598a16c232418a5c9dc7be50aa8a7fb5a9afd624
Shahir Hossain <shahirhossain10@gmail.com>
2023-08-03 17:24:09 +0600

    started to implemented the preview xlsx file

--- src/pages/Bulletin/UploadButtelin/index.jsx
+++ src/pages/Bulletin/UploadButtelin/index.jsx
@@ -1,6 +1,6 @@
 import { InboxOutlined } from "@ant-design/icons";
 import { message, Upload } from "antd";
-import React from "react";
+import React, { useState } from "react";
 const { Dragger } = Upload;
 
 const props = {
@@ -21,15 +21,26 @@ const props = {
   },
   onDrop(e) {
     console.log("Dropped files", e.dataTransfer.files);
+    const reader = new FileReader();
+    reader.readAsBinaryString(e.dataTransfer.files[0]);
+    reader.onload = (e) => {
+      const data = e.target.result;
+      const workbook = XLSX.read(data);
+      const sheetname = workbook.SheetNames[0];
+      const sheet = workbook.sheet(sheetname);
+      const parsedData = XLSX.utils.sheet_to_json(sheet);
+      console.log(parsedData);
+    };
   },
 };
 
 const UploadBulletin = () => {
+  const [previewData, setPreviewData] = useState([]);
   return (
     <>
       <h3 className="text-center text-2xl my-5">Upload Buletin</h3>
       <div className="px-52">
-        <Dragger {...props} className="">
+        <Dragger {...props} className="h-80">
           <p className="ant-upload-drag-icon">
             <InboxOutlined />
           </p>
@@ -41,6 +52,9 @@ const UploadBulletin = () => {
             uploading company data or other banned files.
           </p>
         </Dragger>
+        <div className="mt-4">
+          <span className="text-2xl">Preview: </span>
+        </div>
       </div>
     </>
   );
